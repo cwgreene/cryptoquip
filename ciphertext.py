37cbb5d0bd7692895c4acb343c3d6ef6b7764146
Chris Greene <archgoon@gmail.com>
2022-03-25 17:48:03 -0700

    Update ciphertext to actually work again.

--- ciphertext.py
+++ ciphertext.py
@@ -7,6 +7,12 @@ class CipherText():
             self.source = [("Encoded", s) for s in source]
         self.transformations = []
 
+    def stats(self, **kwargs):
+        counts = {}
+        for code in self.source:
+            counts[code] = counts.get(code,0)+1
+        print(sorted(counts.items(), key=lambda x:x[1]))
+
     def replace(self, a, b):
         for i, (t,s) in enumerate(self.source[:]):
             if t == a[0] and s == a[1]:
@@ -40,7 +46,7 @@ class CipherText():
             return source
         afile = open(cmd[1], "w")
         afile.write(str(self.transformations))
-        return source
+        return self.source
 
     def load_transformations(self, cmd, **kwargs):
         if len(cmd) != 2:
@@ -48,11 +54,20 @@ class CipherText():
             return source
         afile = open(cmd[1])
         new_transformations = ast.literal_eval(afile.read()) # TODO: Make this more secure
-        for i in range(len(transformations)):
-            source = undo(source, transformations, [])
+        for i in range(len(self.transformations)):
+            self.undo()
         for transform in new_transformations:
-            source = new_transformation(source, transformations, transform)
-        return source
+            self.new_transformation((transform[0][1], transform[1][1]))
+        return self.source
+
+    def new_transformation(self, cmd, **kwargs):
+        if ("Decoded", cmd[1]) in self.source:
+            print("Already in use")
+            return
+        transform = (("Encoded", cmd[0]), ("Decoded", cmd[1]))
+        print(transform)
+        self.replace(transform[0], transform[1])
+        self.transformations.append(transform)
 
     def __str__(self):
         acc = ""
